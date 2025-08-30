77571a411fef4e502cda96100faac87e99a09bd5
Kaurvikagupta <kuhugupta469@gmail.com>
2025-08-30 18:48:43 +0530

    Add Initial Files

---
+++ js/index.js
@@ -0,0 +1,38 @@
+console.log("This is my script")
+let result = {
+    "tag": "",
+    "free": true,
+    "role": false,
+    "user": "guptakuhu2315",
+    "email": "guptakuhu2315@gmail.com",
+    "score": 0.64,
+    "state": "deliverable",
+    "domain": "gmail.com",
+    "reason": "valid_mailbox",
+    "mx_found": true,
+    "catch_all": null,
+    "disposable": false,
+    "smtp_check": true,
+    "did_you_mean": "",
+    "format_valid": true
+}
+
+submitBtn.addEventListener("click", async(e) => {
+    e.preventDefault()
+    console.log("Clicked!")
+    resultCont.innerHTML = `<img width="123" src="img/loading.svg" alt="">`
+    let key = "ema_live_JE52nQ7JswdblnGTb0o5CIdml4T9BAJQKY4VlRcn"
+    let email = document.getElementById("username").value
+    let url = `https://api.emailvalidation.io/v1/info?apikey=${key}&email=${email}`
+    let res = await fetch(url)
+    let result = await res.json()
+    let str = ``
+    for (key of Object.keys(result)) {
+        if (result[key] !== "" && result[key] !== " ") {
+            str = str + `<div>${key}: ${result[key]}</div>`
+        }
+    }
+
+    console.log(str)
+    resultCont.innerHTML = str
+})
